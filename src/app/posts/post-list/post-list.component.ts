8055acc4de254d1b9ece81106723aaedd579901d
rachnabalani <balani.rachna@gmail.com>
2018-11-04 15:59:15 -0500

    Adding an Expansion Panel for posts

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -2,10 +2,19 @@ import {Component} from '@angular/core';
 
 @Component({
  selector: 'app-post-list',
- templateUrl: './post-list.component.html'
-
+ templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
 })
 
 
 
-export class PostListComponent {}
+export class PostListComponent {
+  // posts = [
+  //   {title: 'First Post', content: 'This is the First Post\'s Content' },
+  //   {title: 'Second Post', content: 'This is the Second Post\'s Content' },
+  //   {title: 'Third Post', content: 'This is the Third Post\'s Content' },
+  // ];
+
+  posts = [];
+
+}
