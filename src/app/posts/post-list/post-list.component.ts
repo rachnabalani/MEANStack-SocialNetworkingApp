606c8bb1f5b055869ac7bec69e43527a1e3d32bb
rachnabalani <balani.rachna@gmail.com>
2018-11-04 21:35:34 -0500

    Observables and Subscriptions added

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import {Post} from '../post.model';
 import {PostsService} from '../posts.service';
@@ -10,8 +11,7 @@ import {PostsService} from '../posts.service';
 })
 
 
-
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   {title: 'First Post', content: 'This is the First Post\'s Content' },
   //   {title: 'Second Post', content: 'This is the Second Post\'s Content' },
@@ -19,11 +19,21 @@ export class PostListComponent implements OnInit {
   // ];
 
   posts: Post[] = [];
-
+  private postsSub: Subscription;
   constructor( public postsService: PostsService) {}
 
   ngOnInit() {
     this.posts = this.postsService.getPosts();
+
+    this.postsSub = this.postsService.getPostUpdateListener()
+    .subscribe((posts: Post[]) => {
+      this.posts = posts; // equal the Post[] element to the new post we just recieved
+    }); //  3 arguments: function executed,
+    // called when error happens (never happening),
+    // called when observable is completed/no more values expected (never happening)
   }
 
+  ngOnDestroy() {
+    this.postsSub.unsubscribe(); // prevents memory leaks by removing subscription
+  }
 }
